d91beb198de528afc2681d91b4063d09c3e538f3
Rishi Kokil <rishikokil@gmail.com>
2023-10-14 13:35:04 +0530

    Added Blog Template and CSS

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
+import {useState} from "react"
+
 const Home = () => {
+    const [blogs , SetBlogs] = useState([
+        {title : "My New Website", body : "Lorem-ipsum....." , author : "Author1" , id : 1},
+        {title : "My favourite food", body : "Lorem-ipsum....." , author : "Author2" , id : 2},
+        {title : "Movies I like", body : "Lorem-ipsum....." , author : "Author3" , id : 3},
+    ]);
+     
     return ( 
         <div className="home">
-            <h2>Home</h2>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key = {blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Written by {blog.author}</p>
+                </div>
+            ))}
         </div>
      );
 }
