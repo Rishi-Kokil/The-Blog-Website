3ff6b37588c675143f4c87db4b4bd5eaa6dda01b
Rishi Kokil <rishikokil@gmail.com>
2023-10-14 19:59:09 +0530

    Added Fetch API to fetch data from the JSON server

--- src/BlogList.js
+++ src/BlogList.js
@@ -3,7 +3,7 @@
 // we are using destructuring sytax for directly accessing the props object properties
 
 // blogs array is taken as input and title is also taken
-const BlogList = ({ blogs, title, handleClick}) => {
+const BlogList = ({ blogs, title}) => {
     return (
         <div className="blog-list">
             <h2>{title}</h2>
@@ -12,7 +12,6 @@ const BlogList = ({ blogs, title, handleClick}) => {
                     <div className="blog-preview" key={blog.id}>
                         <h2>{blog.title}</h2>
                         <p>Written by {blog.author}</p>
-                        <button onClick={() => { handleClick(blog.id) }}>Click to Delete Blog</button>
                     </div>
                 ))
             }
--- src/Home.js
+++ src/Home.js
@@ -2,18 +2,32 @@ import { useState, useEffect } from "react"
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, SetBlogs] = useState([
-        { title: "My New Website", body: "Lorem-ipsum.....", author: "Author1", id: 1 },
-        { title: "My favourite food", body: "Lorem-ipsum.....", author: "Author2", id: 2 },
-        { title: "Movies I like", body: "Lorem-ipsum.....", author: "Author3", id: 3 },
-    ]);
+    const [blogs, SetBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error , setError] = useState(null);
 
-    const [name, setName] = useState("Mario");
 
     useEffect(
-        () => { console.log("Use effect is triggered"); },
-        [name]
-    );
+        () => {
+            //here a promise is returned by fetch api
+            // we can pass the fuilfillment value by using then method
+
+            fetch("http://localhost:8000/blogs") // local server that is running on our machine
+                .then(res => {
+                    if(!res.ok){ // response object has a property of OK and it is boolean
+                        // when response is successfull then it is set to true else false
+                        throw new Error("Could Not Fetch data")
+                    }
+                    return res.json(); // it is returned if there is no error
+                    
+                })  // .json is used to parse the data into JSON format
+                .then(data => { 
+                    SetBlogs(data);
+                    setIsPending(false);
+                })
+                .catch(err => setError(err.message));
+        }
+        , []); // empty array so that the website does not invoke the anonymous function on everu=y re render
 
     const handleClick = (id) => {
         const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -22,7 +36,15 @@ const Home = () => {
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleClick={handleClick} />
+            {/* Conditional Rendering*/}
+
+            {error && <div>{error}</div>}
+
+
+            {/* Below code runs only when the blogs data is fetched by the fetch api completely */}
+            {/* {isPending && <BlogList blogs={blogs} title="All Blogs!" handleClick={handleClick} />}  */}
+            {/* Alternate Syntax */}
+            {!isPending ? <BlogList blogs={blogs} title="All Blogs!" /> : "Loading...."}
         </div>
     );
 }
